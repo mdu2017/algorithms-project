1e7ac2fb40058c76b64d65767d4f889e59f00995
Mark Du <dum@ECS.BAYLOR.EDU>
2019-04-08 18:20:15 -0500

    Setup files

---
+++ cmake-build-debug/CMakeFiles/Project.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project.dir/testDriver.cpp.obj"
+  "CMakeFiles/Project.dir/color_rgb.cpp.obj"
+  "CMakeFiles/Project.dir/curve.cpp.obj"
+  "CMakeFiles/Project.dir/line.cpp.obj"
+  "CMakeFiles/Project.dir/point.cpp.obj"
+  "CMakeFiles/Project.dir/SDL_Plotter.cpp.obj"
+  "Project.pdb"
+  "Project.exe"
+  "Project.exe.manifest"
+  "libProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
